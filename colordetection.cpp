8aa2ff73a45295966604f633348c184e28e31f59
Z <101237039+AzeMaeve@users.noreply.github.com>
2025-10-27 17:10:07 +1100

    Update colordetection.cpp

--- colordetection.cpp
+++ colordetection.cpp
@@ -5,36 +5,36 @@
 using namespace cv;
 using namespace std;
 
- int main( int argc, char** argv )
- {
+int main(int argc, char** argv)
+{
     VideoCapture cap(0); //capture the video from web cam
 
-    if ( !cap.isOpened() )  // if not success, exit program
+    if (!cap.isOpened())  // if not success, exit program
     {
-         cout << "Cannot open the web cam" << endl;
-         return -1;
+        cout << "Cannot open the web cam" << endl;
+        return -1;
     }
 
-    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+    namedWindow("Control", WINDOW_AUTOSIZE); //create a window called "Control"
 
-  int iLowH = 0;
- int iHighH = 179;
+    int iLowH = 0;
+    int iHighH = 179;
 
-  int iLowS = 0; 
- int iHighS = 255;
+    int iLowS = 0;
+    int iHighS = 255;
 
-  int iLowV = 0;
- int iHighV = 255;
+    int iLowV = 0;
+    int iHighV = 255;
 
-  //Create trackbars in "Control" window
- cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
- cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+    //Create trackbars in "Control" window
+    createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    createTrackbar("HighH", "Control", &iHighH, 179);
 
-  cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
- cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+    createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    createTrackbar("HighS", "Control", &iHighS, 255);
 
-  cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
- cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+    createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+    createTrackbar("HighV", "Control", &iHighV, 255);
 
     while (true)
     {
@@ -42,38 +42,38 @@ using namespace std;
 
         bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 
-         if (!bSuccess) //if not success, break loop
+        if (!bSuccess) //if not success, break loop
         {
-             cout << "Cannot read a frame from video stream" << endl;
-             break;
+            cout << "Cannot read a frame from video stream" << endl;
+            break;
         }
 
-    Mat imgHSV;
+        Mat imgHSV;
 
-   cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
- 
-  Mat imgThresholded;
+        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
 
-   inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-      
-  //morphological opening (remove small objects from the foreground)
-  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+        Mat imgThresholded;
 
-   //morphological closing (fill small holes in the foreground)
-  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
 
-   imshow("Thresholded Image", imgThresholded); //show the thresholded image
-  imshow("Original", imgOriginal); //show the original image
+        //morphological opening (remove small objects from the foreground)
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+        dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+        //morphological closing (fill small holes in the foreground)
+        dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+        imshow("Thresholded Image", imgThresholded); //show the thresholded image
+        imshow("Original", imgOriginal); //show the original image
 
         if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-       {
+        {
             cout << "esc key is pressed by user" << endl;
-            break; 
-       }
+            break;
+        }
     }
 
-   return 0;
+    return 0;
 
 }
